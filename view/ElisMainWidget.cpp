93e3748f3e7bf831d01871254f341de8b3f1f61b
chiplua <chiplua@163.com>
2021-08-18 15:43:54 +0800

    [UI-Framework-TextEditor]:Change text editor background color

--- view/ElisMainWidget.cpp
+++ view/ElisMainWidget.cpp
@@ -21,6 +21,19 @@ ElisMainWidget::ElisMainWidget(QWidget *parent) :
     ui->verticalLayoutWidget_7->setStyleSheet("background:rgb(211, 211, 211)");
     ui->gridLayoutWidget->setStyleSheet("background:rgb(211, 211, 211)");
     ui->gridLayoutWidget_2->setStyleSheet("background:rgb(211, 211, 211)");
+
+    ui->tvExitTotal->setStyleSheet("background:white");
+    ui->tvEntryTotal->setStyleSheet("background:white");
+    ui->tvFlapClosingTimeout->setStyleSheet("background:white");
+    ui->tvFlashPeriod->setStyleSheet("background:white");
+    ui->tvLeaveAisleTimeout->setStyleSheet("background:white");
+    ui->tvEntryTotal->setStyleSheet("background:white");
+    ui->tvLightDuration->setStyleSheet("background:white");
+    ui->tvMaxPassage->setStyleSheet("background:white");
+    ui->tvOpenSafetyTimeout->setStyleSheet("background:white");
+    ui->tvPassengerAuthorizationTimeout->setStyleSheet("background:white");
+    ui->tvSensorTimeout->setStyleSheet("background:white");
+    ui->tvStressTestIntervalTime->setStyleSheet("background:white");
 }
 
 ElisMainWidget::~ElisMainWidget() {
