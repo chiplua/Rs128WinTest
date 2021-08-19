a9a8f573619f3b95138d6982aa0b591b5bbe6847
chiplua <chiplua@163.com>
2021-08-19 13:18:11 +0800

    [UI-Framework-Button]:Set buttons text color

--- src/view/ElisMainWidget.cpp
+++ src/view/ElisMainWidget.cpp
@@ -7,13 +7,23 @@
 #include "ElisMainWidget.h"
 #include "ui_ElisMainWidget.h"
 
+QPalette getButtonCommonPalette() {
+    QPalette pal;
+    pal.setColor(QPalette::ButtonText, QColor(255,255,255));
+
+    return pal;
+}
 void setHLayoutCom(Ui::ElisMainWidget *ui) {
     ui->horizontalLayoutWidget->setStyleSheet("background:rgb(211, 211, 211)");
     ui->hLayoutCom->setSpacing(20);
     ui->btnOpenCom->setStyleSheet("background:gray");
+    ui->btnOpenCom->setPalette(getButtonCommonPalette());
     ui->btnCloseCom->setStyleSheet("background:gray");
+    ui->btnCloseCom->setPalette(getButtonCommonPalette());
     ui->btnCurrentStatusRequest->setStyleSheet("background:gray");
+    ui->btnCurrentStatusRequest->setPalette(getButtonCommonPalette());
     ui->btnOk->setStyleSheet("background:gray");
+    ui->btnOk->setPalette(getButtonCommonPalette());
 }
 
 void setVLayoutPassengersLeft(Ui::ElisMainWidget *ui) {
@@ -53,6 +63,7 @@ void setVLayoutPassengersRight(Ui::ElisMainWidget *ui) {
     ui->tvOpenSafetyTimeout->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     ui->tvOpenSafetyTimeout->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     ui->btnParameterSetting->setStyleSheet("background:gray");
+    ui->btnParameterSetting->setPalette(getButtonCommonPalette());
 }
 
 void setHLayoutPassengers(Ui::ElisMainWidget *ui) {
@@ -66,6 +77,7 @@ void setVLayoutModeSetting(Ui::ElisMainWidget *ui) {
     ui->verticalLayoutWidget_2->setStyleSheet("background:rgb(211, 211, 211)");
     ui->btnGateModeSetting->setStyleSheet("background:gray");
     ui->btnGateModeSetting->setFixedSize((ui->verticalLayoutWidget_2->width() - 30), 20);
+    ui->btnGateModeSetting->setPalette(getButtonCommonPalette());
     ui->vLayoutModeSetting->setContentsMargins(20, 40, 20, 0);
 }
 
@@ -73,6 +85,7 @@ void setVLayoutAisleMode(Ui::ElisMainWidget *ui) {
     ui->verticalLayoutWidget_3->setStyleSheet("background:rgb(211, 211, 211)");
     ui->btnAisleModeSetting->setStyleSheet("background:gray");
     ui->btnAisleModeSetting->setFixedSize((ui->verticalLayoutWidget_6->width() - 40), 20);
+    ui->btnAisleModeSetting->setPalette(getButtonCommonPalette());
     ui->vLayoutAisleMode->setContentsMargins(20, 0, 20, 10);
 }
 
@@ -81,6 +94,7 @@ void setVLayoutTestParam(Ui::ElisMainWidget *ui) {
     ui->tvTestParam->setFixedSize(40, 30);
     ui->btnTestModule->setStyleSheet("background:gray");
     ui->btnTestModule->setFixedSize((ui->verticalLayoutWidget_4->width() - 30), 20);
+    ui->btnTestModule->setPalette(getButtonCommonPalette());
     ui->vLayoutTestParam->setContentsMargins(20, 20, 20, 25);
 }
 
@@ -97,12 +111,14 @@ void setGLayoutLightSetting(Ui::ElisMainWidget *ui) {
     ui->tvFlapClosingTimeout->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 
     ui->btnTopIndicatorLight->setStyleSheet("background:gray");
+    ui->btnTopIndicatorLight->setPalette(getButtonCommonPalette());
 }
 
 void setPassengerAuthorization(Ui::ElisMainWidget *ui) {
     ui->verticalLayoutWidget_5->setStyleSheet("background:rgb(211, 211, 211)");
     ui->btnPassageAuthorization->setStyleSheet("background:gray");
     ui->btnPassageAuthorization->setFixedSize((ui->verticalLayoutWidget_5->width() - 30), 20);
+    ui->btnPassageAuthorization->setPalette(getButtonCommonPalette());
     ui->vLayoutPassengerAuthorization->setContentsMargins(20, 0, 20, 15);
 }
 
@@ -111,7 +127,9 @@ void setGLayoutStressTest(Ui::ElisMainWidget *ui) {
     ui->gLayoutStressTest->setContentsMargins(20, 20, 20, 20);
 
     ui->btnStartStressTest->setStyleSheet("background:gray");
+    ui->btnStartStressTest->setPalette(getButtonCommonPalette());
     ui->btnStopStressTest->setStyleSheet("background:gray");
+    ui->btnStopStressTest->setPalette(getButtonCommonPalette());
 
     ui->tvStressTestIntervalTime->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     ui->tvStressTestIntervalTime->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
@@ -127,11 +145,24 @@ void setNormalFlashGedMode(Ui::ElisMainWidget *ui) {
     ui->verticalLayoutWidget_6->setStyleSheet("background:rgb(211, 211, 211)");
     ui->btnGedMode->setStyleSheet("background:gray");
     ui->btnGedMode->setFixedSize((ui->verticalLayoutWidget_5->width() - 30), 20);
+    ui->btnGedMode->setPalette(getButtonCommonPalette());
     ui->vLayoutNormalFlashGedMode->setContentsMargins(20, 0, 20, 15);
 }
 
 void setRightButtons(Ui::ElisMainWidget *ui) {
-
+    ui->verticalLayoutWidget_7->setStyleSheet("background:rgb(211, 211, 211)");
+    ui->btnVersionRequest->setStyleSheet("background:gray");
+    ui->btnVersionRequest->setPalette(getButtonCommonPalette());
+    ui->btnSetEmergency->setStyleSheet("background:gray");
+    ui->btnSetEmergency->setPalette(getButtonCommonPalette());
+    ui->btnCancelEmergency->setStyleSheet("background:gray");
+    ui->btnCancelEmergency->setPalette(getButtonCommonPalette());
+    ui->btnClearPassageCount->setStyleSheet("background:gray");
+    ui->btnClearPassageCount->setPalette(getButtonCommonPalette());
+    ui->btnSensortest->setStyleSheet("background:gray");
+    ui->btnSensortest->setPalette(getButtonCommonPalette());
+    ui->btnClearInfo->setStyleSheet("background:gray");
+    ui->btnClearInfo->setPalette(getButtonCommonPalette());
 }
 
 ElisMainWidget::ElisMainWidget(QWidget *parent) :
@@ -148,7 +179,6 @@ ElisMainWidget::ElisMainWidget(QWidget *parent) :
     setNormalFlashGedMode(ui);
     setRightButtons(ui);
 
-    ui->verticalLayoutWidget_7->setStyleSheet("background:rgb(211, 211, 211)");
 
     ui->tvExitTotal->setStyleSheet("background:white");
     ui->tvEntryTotal->setStyleSheet("background:white");
