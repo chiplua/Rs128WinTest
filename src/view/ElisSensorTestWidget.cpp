12f7b90eab423bdf553ddeebfa8c75bd6d79eaea
chiplua <chiplua@163.com>
2021-08-25 11:06:12 +0800

    [LABLE-Font-Size]:Set QLable font size

--- src/view/ElisSensorTestWidget.cpp
+++ src/view/ElisSensorTestWidget.cpp
@@ -47,7 +47,7 @@ void ElisSensorTestWidget::setHLayoutButtons(Ui::ElisSensorTestWidget *ui) {
 }
 
 void setLeftSensors(Ui::ElisSensorTestWidget *ui, QString s1Color, QString s2Color, QString s3Color, QString s4Color, QString s5Color) {
-    const QString styleBaseStr = "min-width:20px;min-height:20px;max-width:20px;max-height:20px;border-radius:10px;border:1px solid black;background:";
+    const QString styleBaseStr = "font-size:10px;min-width:20px;min-height:20px;max-width:20px;max-height:20px;border-radius:10px;border:1px solid black;background:";
     ui->lbS1->setStyleSheet(styleBaseStr + s1Color);
     ui->lbS2->setStyleSheet(styleBaseStr + s2Color);
     ui->lbS3->setStyleSheet(styleBaseStr + s3Color);
@@ -56,7 +56,7 @@ void setLeftSensors(Ui::ElisSensorTestWidget *ui, QString s1Color, QString s2Col
 }
 
 void setMiddle1Sensors(Ui::ElisSensorTestWidget *ui, QString s6Color, QString s7Color, QString s8Color, QString s9Color) {
-    const QString styleBaseStr = "min-width:20px;min-height:20px;max-width:20px;max-height:20px;border-radius:10px;border:1px solid black;background:";
+    const QString styleBaseStr = "font-size:10px;min-width:20px;min-height:20px;max-width:20px;max-height:20px;border-radius:10px;border:1px solid black;background:";
     ui->lbS6->setStyleSheet(styleBaseStr + s6Color);
     ui->lbS7->setStyleSheet(styleBaseStr + s7Color);
     ui->lbS8->setStyleSheet(styleBaseStr + s8Color);
@@ -64,7 +64,7 @@ void setMiddle1Sensors(Ui::ElisSensorTestWidget *ui, QString s6Color, QString s7
 }
 
 void setMiddle2Sensors(Ui::ElisSensorTestWidget *ui, QString s10Color, QString s11Color, QString s12Color, QString s13Color) {
-    const QString styleBaseStr = "min-width:20px;min-height:20px;max-width:20px;max-height:20px;border-radius:10px;border:1px solid black;background:";
+    const QString styleBaseStr = "font-size:10px;min-width:20px;min-height:20px;max-width:20px;max-height:20px;border-radius:10px;border:1px solid black;background:";
     ui->lbS10->setStyleSheet(styleBaseStr + s10Color);
     ui->lbS11->setStyleSheet(styleBaseStr + s11Color);
     ui->lbS12->setStyleSheet(styleBaseStr + s12Color);
@@ -72,7 +72,7 @@ void setMiddle2Sensors(Ui::ElisSensorTestWidget *ui, QString s10Color, QString s
 }
 
 void setRightSensors(Ui::ElisSensorTestWidget *ui, QString s14Color, QString s15Color, QString s16Color, QString s17Color, QString s18Color) {
-    const QString styleBaseStr = "min-width:20px;min-height:20px;max-width:20px;max-height:20px;border-radius:10px;border:1px solid black;background:";
+    const QString styleBaseStr = "font-size:10px;min-width:20px;min-height:20px;max-width:20px;max-height:20px;border-radius:10px;border:1px solid black;background:";
     ui->lbS14->setStyleSheet(styleBaseStr + s14Color);
     ui->lbS15->setStyleSheet(styleBaseStr + s15Color);
     ui->lbS16->setStyleSheet(styleBaseStr + s16Color);
